541744f8bb16c6abfbfea2ef26a0cf1785c914db
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-24 16:34:24 -0800

    Adds Application Factory (flaskr) with some comments

---
+++ flaskr/__init__.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+from dotenv import dotenv_values
+
+# this isn't actually necessary if using an instance config file
+# but wanted to try it out
+config = dotenv_values(".env")
+
+def create_app(test_config=None):
+    # create and configure the app
+    ## __name__: name of current Python module
+    ## instance_relative_config=True
+    app = Flask(__name__, instance_relative_config=True) # create Flask instance
+    app.config.from_mapping(
+        SECRET_KEY=config['SECRET_KEY'],
+        DATABASE=config['DATABASE'],
+    )
+
+    # test_config: a dedicated config for testing
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    ## this is where the SQLite database file will be created
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
