9ed4bdf9b6c32f07587393a329f20bd12ae317f5
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-18 14:18:48 -0700

    Adds helper function get_post (by id)

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -42,3 +42,19 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+post = get_db().execute(
+    'SELECT p.id, title, body, created, author_id, username'
+    ' FROM post p JOIN user u ON p.author_id = u.id'
+    ' WHERE p.id = ?',
+    (id,)
+).fetchone()
+
+if post is None:
+    abort(404, "Post id {0} doesn't exist.".format(id))
+
+if check_author and post['author_id'] != g.user['id']:
+    abort(403)
+
+return post
