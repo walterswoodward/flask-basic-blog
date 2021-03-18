34ffa1c5b938a508121a90a5551fcbbdfc3e5e0d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-03-18 13:33:17 -0700

    Cleanup/simplify routes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,13 +35,11 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    @app.route('/catz')
-    def catz():
-        return 'MeowMeow'
+    @app.route('/')
+    def is_active():
+        return {
+            "active": "true"
+        }
 
     from . import auth
     app.register_blueprint(auth.bp)
